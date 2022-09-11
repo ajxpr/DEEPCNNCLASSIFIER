5a95876f02b152eb26d040769244374182f82240
thezillionaireboy <akshaj.piri@gmail.com>
2022-09-11 12:32:34 +0530

    commit

--- init_setup.sh
+++ init_setup.sh
@@ -1,8 +1,8 @@
-echo [$(date)]: "START"
-echo [$(date)]: "Creating environment with Python 3.8 version"
+echo [$(date)]: "START" 
+echo [$(date)]: "creating env with python 3.8 version" 
 conda create --prefix ./env python=3.8 -y
-echo [$(date)]: "Activating the environment"
+echo [$(date)]: "activating the environment" 
 source activate ./env
-echo [$(date)]: "Installing the devenvironment"
+echo [$(date)]: "installing the dev requirements" 
 pip install -r requirements_dev.txt
-echo [$(date)]: "END"
+echo [$(date)]: "END" 
