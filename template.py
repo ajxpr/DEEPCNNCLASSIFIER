a41181f6f38be81ae9432120508e67cdfc52ed8d
thezillionaireboy <akshaj.piri@gmail.com>
2022-09-11 11:27:20 +0530

    added tests

--- template.py
+++ template.py
@@ -10,24 +10,29 @@ logging.basicConfig(
 package_name = "deepClassifier"
 
 list_of_files = [
-    ".github/workflows/.gitkeep"
-    f"src/{package_name}/__init__.py",
-    f"src/{package_name}/compnents/__init__.py",
-    f"src/{package_name}/utils/__init__.py",
-    f"src/{package_name}/config/__init__.py",
-    f"src/{package_name}/pipeline/__init__.py",
-    f"src/{package_name}/entity/__init__.py",
-    f"src/{package_name}/constant/__init__.py",
-    "config/config.yaml",
-    "dvc.yaml",
-    "params.yaml",
-    "init_setup.sh",
-    "requirements.txt",
-    "requirements_dev.txt",
-    "setup.py",
-    "pyproject.toml",
-    "tox.ini",
-    "research/trails.ipynb"
+   ".github/workflows/.gitkeep",
+   f"src/{package_name}/__init__.py", 
+   f"src/{package_name}/components/__init__.py", 
+   f"src/{package_name}/utils/__init__.py", 
+   f"src/{package_name}/config/__init__.py", 
+   f"src/{package_name}/pipeline/__init__.py", 
+   f"src/{package_name}/entity/__init__.py", 
+   f"src/{package_name}/constants/__init__.py",
+   "tests/__init__.py",
+   "tests/unit/__init__.py",
+   "tests/integration/__init__.py",
+   "configs/config.yaml",
+   "dvc.yaml",
+   "params.yaml",
+   "init_setup.sh",
+   "requirements.txt", 
+   "requirements_dev.txt",
+   "setup.py",
+   "setup.cfg",
+   "pyproject.toml",
+   "tox.ini",
+   "research/trials.ipynb" 
+   "example.py"
 ]
 
 for filepath in list_of_files:
