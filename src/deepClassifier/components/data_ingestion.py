1f26aee7ae9a818fe0dee1b2b17c590756567d3c
thezillionaireboy <akshaj.piri@gmail.com>
2022-10-01 21:58:56 +0530

    updated

--- src/deepClassifier/components/data_ingestion.py
+++ src/deepClassifier/components/data_ingestion.py
@@ -7,7 +7,6 @@ from deepClassifier.utils import get_size
 from tqdm import tqdm
 from pathlib import Path
 
-
 class DataIngestion:
     def __init__(self, config: DataIngestionConfig):
         self.config = config
